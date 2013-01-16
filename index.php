610bdbd59e7ae44124ae9c6a520f244b296bd15e
Adam Balsam <adam.balsam@brunellocreative.com>
2013-01-16 14:02:15 -0500

    Simplified JS

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
   </head>
   <body>
     <h1>Excuse Me!</h1>
+    
     <?php
+    // If we have POST data, we know that the form as just submitted. Fire the
+    // missile and display a success message.
     if (!empty($_POST['name'])) {
       $fire = shell_exec('sudo python ' . $_SERVER['DOCUMENT_ROOT'] . '/attack.py ' . $_POST['name']);
       echo '<h2>Successfully Fired on ' . $_POST['name'] . '</h2>';
     }
     ?>
     
-    <p>Is Jill zoned out? Does Kyle have his headphones on? Select the person who isn't paying attention to you and click "Fire!".</p>
+    <p>Is Jill zoned out? Does Kyle have his headphones on? Select the person who isn't paying attention to you and click "Fire!"</p>
     
     <form action="" id="attack-form" method="POST">
       <select name="name">
@@ -23,38 +26,48 @@
         <option value="adam">Adam</option>
         <option value="john">John</option>
       </select>
-      <br />
       <input type="submit" id="form-submit" value="Fire!" />
       <p><em>Allow up to 15 seconds for the system to acquire missle lock on the target.</em></p>
     </form>
+    <div id="test">fds</div>
 
     <p>Are you trying to get the attention of someone who isn't on the list? Unfortunately, our missle launcher has limited range and is easily deflected by materials like glass.</p>
     
     <div id="status">Please Standby. Acquiring Target...</div>
-
+    
     <script>
+    // Eye candy
     $(document).ready(function() {
-      $('#form-submit').click(function() {
+      /**
+       * Array to define status messages after the Fire button is clicked
+       *   @key - number of miliseconds after button is clicked
+       *   @value - the message that will be displayed
+       */
+      var statuses = {
+        '3000' : 'Calculating coordinates...',
+        '6100' : 'Adjusting for coriolis effect...',
+        '9300' : 'Rounding up to the nearest 10...',
+        '11000' : 'Firing!', // About 11 seconds for mine to start firing
+      }
+      
+      // On click of the fire button, fade in the overlay and cycle through
+      // the status messages until the page reloads
+      $('#test').click(function() {
         $('#status').fadeIn();
-        setTimeout(function() {
-          updateStatus('Calculating coordinates...');
-        }, 3000);
-        setTimeout(function() {
-          updateStatus('Adjusting for coriolis effect...');
-        }, 6100);
-        setTimeout(function() {
-          updateStatus('Rounding up to the nearest 10...');
-        }, 9300);
-        setTimeout(function() {
-          updateStatus('Firing!');
-        }, 11000);
+        $.each(statuses, function( delay, message ) {
+          setTimeout(function() {
+            updateStatus(message);
+          }, delay);
+        });
       });
-      function updateStatus(text) {
-        $('#status').text(text);
+      
+      // Function that actually replaces the text in #status (overlay) div
+      function updateStatus(message) {
+        $('#status').text(message);
       }
     });
-    
     </script>
 
   </body>
 </html>
+
